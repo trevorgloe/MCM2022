c92e1f4a5d82715ff59da6a17a69012c52241168
unknown <trevorgloe@gmail.com>
2022-02-18 11:56:49 -0800

    organized parameters into structs

--- params_discret.m
+++ params_discret.m
@@ -7,11 +7,23 @@ g = 9.81;   % acceleration due to gravity [m/s^2]
 m = 1;      % rider mass [kg]
 Cr = 0.1;   % wheel resistance coefficient
 Cd = 0.1;   % air drag coefficient
+CP = 100;   % rider critical power
+Wp = 100;   % rider anaerobic work capacity
+
+biker.m = m;
+biker.Cr = Cr;
+biker.Cd = Cd;
+biker.CP = CP;
+biker.Wp = Wp;
+
 
 %% Define course parameters
 L = 100;    %total course length [m]
 phi = [1 2 3 4 6 7 8 7 6 5 4 3 2 1];   %angle of the slope of the course over the length of the course
 
+%structure to store course parameters
+course.L = L;
+course.phi = phi;
 
 %% Discretize course into lil chunky bits
 N = 100;    %number of chunks in discretization

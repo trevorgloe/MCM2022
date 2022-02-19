f81ec01ba03ce69a1e4af0e9560714692dbe23bb
mllytle <madilytle@gmail.com>
2022-02-19 14:13:40 -0800

    editing and want to pull

--- model_validation_1.m
+++ model_validation_1.m
@@ -8,16 +8,16 @@ g = 9.81;   % acceleration due to gravity [m/s^2]
 
 % Define biker parameters
 biker.m = 66.25;      % rider mass [kg]
-biker.Cr = 0.005;   % wheel resistance coefficient
-biker.CdA = 0.194*0.1;   % drag coeff of area [m^2]   (drag coefficient of 0.1 is also in there)
+biker.Cr = 0.002;   % wheel resistance coefficient
+biker.CdA = 0.172;   % drag coeff of area [m^2]   (drag coefficient of 0.1 is also in there)
 biker.CP = 180;   % rider critical power [Watts]
-biker.Wcap = 24235;   % rider anaerobic work capacity [J]
+biker.Wcap = 128e3;   % rider anaerobic work capacity [J]
 biker.tau_w = 500;   % W' recovery time constant
-biker.Pm = 350; % max power [Watts]
+biker.Pm = 1044; % max power [Watts]
 
 % Define course parameters
 course.L = 13e3;    %total course length [m]
-course.phi = [1 2 3 4 6 7 8 7 6 5 4 3 2 1];   %angle of the slope of the course over the length of the course
+course.phi = [0 1 0];%   %angle of the slope of the course over the length of the course
 course.rho = 1.1455; % density at location [kg/m^3]
 
 % Discretize course into lil chunky bits

330566b424b8007f3c59a6fc9ab02c234dcbdd1d
mllytle <madilytle@gmail.com>
2022-02-19 15:20:19 -0800

    added curvature consideration

---
+++ convert_v2.m
@@ -0,0 +1,55 @@
+%% Converts and output of the nonlinear optimization, a velocity vector to a
+% vector of power and calculates the total time
+% must have velocity vector v defined
+% version 2: includes implementation of curvature consideration
+
+m = biker.m;
+Cr = biker.Cr0*sqrt(v.^4 +g^2.*course.r_c.^2)./(g.*course.r_c);
+%     Cd = biker.Cd;
+CP = biker.CP;
+Wcap = biker.Wcap;
+tau_w = biker.tau_w;
+CdA = biker.CdA;
+Pm = biker.Pm;
+
+L = course.L;
+phi = course.phi;
+rho = course.rho;
+
+N = disc.N;
+
+dx = L/N;
+v0 = ones(1,N);
+x = linspace(0,L,N);
+phi_dis = interp1(linspace(0,L,length(phi)),phi,x);
+g = 9.8; % m/s
+
+c1 = 0.5*rho*CdA;% air drag coefficient *** get equation
+c2 = m.*g.*(sind(phi_dis) + Cr);
+c3 = m; % get eq ***
+
+dvdt(1) =  (v(1))/(dx*v(1));
+for ii = 2:N
+    dvdt(ii) = (v(ii) - v(ii-1))/(dx*v(ii));
+end
+% ineq constraint 1
+Pcalc = (c1.*v.^2 + c2 + c3.*dvdt).*v; 
+
+P_diff = Pcalc - P;
+delta_v = delta(P,CP);
+        
+% check to see how much W' was actually used
+x2 = repmat(x(1:30),1,ceil(length(x)/30));
+shifted_x = x2(1:N);
+        
+Wptot = Wcap;
+%         Wexp = 0;
+Wptot = Wcap - sum((1-delta_v+delta_v.*exp(-shifted_x./(v*tau_w))).*(P-CP)*dx);
+
+% total time
+Tf = cumtrapz(x,1./v);
+disp(['Total time: ' num2str(Tf(end)/60)])
+
+function d = delta(P,CP)
+    d = P < CP;
+end
--- model_validation_1.m
+++ model_validation_1.m
@@ -8,24 +8,24 @@ g = 9.81;   % acceleration due to gravity [m/s^2]
 
 % Define biker parameters
 biker.m = 66.25;      % rider mass [kg]
-biker.Cr = 0.002;   % wheel resistance coefficient
+biker.Cr0 = 0.002;   % wheel resistance coefficient
 biker.CdA = 0.172;   % drag coeff of area [m^2]   (drag coefficient of 0.1 is also in there)
 biker.CP = 180;   % rider critical power [Watts]
 biker.Wcap = 128e3;   % rider anaerobic work capacity [J]
 biker.tau_w = 500;   % W' recovery time constant
-biker.Pm = 1044; % max power [Watts]
+biker.Pm = 300; % max power [Watts]
 
 % Define course parameters
 course.L = 13e3;    %total course length [m]
 course.phi = [0 1 0];%   %angle of the slope of the course over the length of the course
 course.rho = 1.1455; % density at location [kg/m^3]
-
+course.r_c = 20*ones(1,100);% curvature from ma boi callan***
 % Discretize course into lil chunky bits
 disc.N = 100;    %number of chunks in discretization
 
 %% Run Model
-[v,P,x] = sqp_run_new(course, biker, disc);
-convert_v;
+[v,P,x] = sqp_run_5(course, biker, disc);
+convert_v2;
 time_values = Tf(end);
 all_params = {biker, course, disc};
 
